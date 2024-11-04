2e078b4a84dc5ec22131d29fa803560a43485e91
Ghilezan19 <madalin.ghilezan@student.upt.ro>
2024-11-04 13:08:15 +0200

    push

---
+++ src/main/java/Documentatie/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Main"},{"p":"Pb2","l":"MainApp"},{"p":"Pb2","l":"Mobilier"},{"p":"Pb2","l":"Orientare"},{"p":"Pb2","l":"Placa"}];updateSearchResults();
